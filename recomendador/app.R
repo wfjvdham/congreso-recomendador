effb7ed4274a06e9948d6999c852dd06d1bbda57
wim <wfjvdham@gmail.com>
2018-01-24 13:30:57 -0500

    removed the ^2 from calculating the table to stay consistend with the graph, use the theme weights in calculating the distance in the graph

--- recomendador/app.R
+++ recomendador/app.R
@@ -14,8 +14,7 @@ example_person_data <- data_frame(
 example_question_data <- data_frame(
   question_nr = c(1, 2),
   thema = c("thema_1", "thema_2"),
-  question = c("Que?", "Como?"),
-  user = c(1, 2)
+  question = c("Que?", "Como?")
 )
 
 NUM_PAGES <- 3
@@ -87,7 +86,6 @@ server <- function(input, output) {
     )
   })
 
-
   output$table <- renderTable({
     example_question_data$user <- 1:NUM_QUESTIONS %>%
       map(function(i) {
@@ -97,25 +95,33 @@ server <- function(input, output) {
     example_question_data %>%
       mutate(thema_wheight = ifelse(thema %in% rv$themas, 3, 1)) %>%
       merge(example_person_data, by = "question_nr") %>%
-      mutate(diff = (as.numeric(user) - question_position) ^ 2 * thema_wheight) %>%
+      mutate(diff = (as.numeric(user) - question_position) * thema_wheight) %>%
       group_by(name) %>%
       summarise(score = sum(diff)) %>%
       arrange(score)
   })
 
   output$plot <- renderPlot({
-    user_position <- 1:NUM_QUESTIONS %>%
+    unweighted_positions_df <- 1:NUM_QUESTIONS %>%
       map(function(i) {
         rv[[paste0(i, "-question")]]
       }) %>%
       set_names(1:NUM_QUESTIONS) %>%
       as_data_frame() %>%
-      mutate(name = "You")
+      gather(question_nr, question_position) %>%
+      mutate(
+        name = "You",
+        question_nr = as.numeric(question_nr),
+        question_position = as.numeric(question_position)
+      ) %>%
+      rbind(example_person_data)
 
-    positions_df <- example_person_data %>%
-      spread(question_nr, question_position) %>%
-      rbind(user_position) %>%
-      as.data.frame()
+    positions_df <- example_question_data %>%
+      mutate(thema_wheight = ifelse(thema %in% rv$themas, 3, 1)) %>%
+      merge(unweighted_positions_df, by = "question_nr") %>%
+      mutate(question_position = question_position * thema_wheight) %>%
+      select(name, question_nr, question_position) %>%
+      spread(question_nr, question_position)
 
     rownames(positions_df) <- positions_df$name
 
